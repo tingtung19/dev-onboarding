396129886a24df475fc3921d06722a33afd489ea
Wawan <wawanqurniawan19@gmail.com>
2023-01-24 17:29:04 +0700

    commit hari ke 8

--- wp-content/themes/mi-starter-theme-elementor-clean/includes/custom-menu-bar/custom-menu-bar.php
+++ wp-content/themes/mi-starter-theme-elementor-clean/includes/custom-menu-bar/custom-menu-bar.php
@@ -4,30 +4,45 @@ defined( 'ABSPATH' ) || die( "Can't access directly" );
 class adminMenuBar{
     public function __construct()
     {
-        add_action( 'admin_menu', 'add_new_menu_items');
+        add_action( 'admin_menu', [$this,'add_new_menu_items']);
     }
-
 
     public function add_new_menu_items () {
-        add_menu_page(
-            "Theme Option",
-            "Theme Option Page",
-            "manage_options",
-            "theme-options",
-            "theme_options_page",
-            100
-        );
-        add_submenu_page(
-            'theme-options',
-            'wpautop-control',
-            'wpautop control',
-            'manage_options',
-            'wpautop-control-menu',
-            'wpautop_control_options'
-        );
-    }
+		add_menu_page(
+			"Users",
+			"Users Page",
+			"manage_options",
+			"users-menu",
+			"users_page",
+			"dashicons-admin-users",
+			5
+		);
+		add_submenu_page(
+			'users-menu',
+			'list-user',
+			'List Users',
+			'manage_options',
+			'users-menu',
+			'users_page'
+		);
+		add_submenu_page(
+			'users-menu',
+			'add-user',
+			'Add Users',
+			'manage_options',
+			'users-add',
+			'users_add_page'
+		);
+	
+
+		
+	}
+
+	
+	
+	
 
 }
 
-// new adminMenuBar;
+new adminMenuBar;
 
